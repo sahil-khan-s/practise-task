3fcc331533c14f582156509c18a40bb0545307db
sahil-khan-s <muhammad.ismaeel5162@gmail.com>
2022-10-20 16:28:14 -0700

    input field added

--- index.js
+++ index.js
@@ -3,11 +3,34 @@ let boxes =document.querySelectorAll('.box')
 Array.from(boxes).forEach((box)=>{
     box.addEventListener('click',(e)=>{
         string = string + e.target.value
-        if(string.length == 4 && string=="1994"){
-            alert('Your password is correct')
-        } else if(string.length == 4 && string!=="1994"){
-         alert("Oops wrong password")
-            }
+       document.querySelector('input').value= string;
+        
             console.log(string)
+        })
+        
     })
-})
+    
+    function myFunction() {
+      if(string.length == 4 && string=="1994"){
+                        alert('Your password is correct')
+                    } else if(string.length == 4  && string!=="1994"){
+                     alert("Oops wrong password")
+                        } 
+      }
+
+
+
+// var string ="";
+// let boxes =document.querySelectorAll('.box')
+// Array.from(boxes).forEach((box)=>{
+//     box.addEventListener('click',(e)=>{
+//         string = string + e.target.value
+//         if(string.length == 4 && string=="1994"){
+//             alert('Your password is correct')
+//         } else if(string.length == 4 && string!=="1994"){
+//          alert("Oops wrong password")
+//             }
+//             console.log(string)
+//     })
+// })
+
