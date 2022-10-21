f72e2646e97ce0e4de6e2cd5320103ac8aa451dd
sahil-khan-s <muhammad.ismaeel5162@gmail.com>
2022-10-21 09:54:13 -0700

    backspace added

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
 var string ="";
-let boxes =document.querySelectorAll('.box')
+var boxes =document.querySelectorAll('.box')
 Array.from(boxes).forEach((box)=>{
     box.addEventListener('click',(e)=>{
         string = string + e.target.value
        document.querySelector('input').value= string;
         
+      
        if(string.length==4){
            alert("You have reached ur limit")
        }
             console.log(string)
         })
-        
+       
     })
     
+
+        //    code for checking the pin 
 
     function myFunction() {
       if(string.length == 4 && string=="1994"){
                         alert('Your password is correct')
                     } else if(string.length == 4  && string!=="1994"){
-                     alert("Oops wrong password")
+                     alert("Oops Wrong Password")
                         } 
       }
+    
+
+    //   code for backspace
+
+      function delete_num() {
+        var input, num;
+        input = document.getElementById('number_input');
+        num = input.value;
+        input.value = num.substr(0,num.length-1);
+        return false;
+      }
+
 
 
 
